65a67726088a5e190ca04a8939f3c8dbc5543ac8
wutsi <herve.tchepannou@wutsi.com>
2022-10-29 16:50:57 -0400

    log language

--- src/main/kotlin/com/wutsi/membership/access/delegate/GetCategoryDelegate.kt
+++ src/main/kotlin/com/wutsi/membership/access/delegate/GetCategoryDelegate.kt
@@ -2,17 +2,21 @@ package com.wutsi.membership.access.delegate
 
 import com.wutsi.membership.access.dto.GetCategoryResponse
 import com.wutsi.membership.access.service.CategoryService
+import com.wutsi.platform.core.logging.KVLogger
 import org.springframework.stereotype.Service
 import javax.servlet.http.HttpServletRequest
 
 @Service
 class GetCategoryDelegate(
     private val service: CategoryService,
-    private val request: HttpServletRequest
+    private val request: HttpServletRequest,
+    private val logger: KVLogger
 ) {
     fun invoke(id: Long): GetCategoryResponse {
         val category = service.findById(id)
         val language = request.getHeader("Accept-Language")
+        logger.add("language", language)
+
         return GetCategoryResponse(
             category = service.toCategory(category, language)
         )
