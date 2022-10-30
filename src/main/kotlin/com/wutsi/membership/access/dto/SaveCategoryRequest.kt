27c70e1b33aa75a029e100138831f2e503c11df2
wutsi <herve.tchepannou@wutsi.com>
2022-10-30 04:43:29 -0400

    update api

--- src/main/kotlin/com/wutsi/membership/access/dto/SaveCategoryRequest.kt
+++ src/main/kotlin/com/wutsi/membership/access/dto/SaveCategoryRequest.kt
@@ -1,7 +1,9 @@
 package com.wutsi.membership.access.dto
 
+import javax.validation.constraints.NotBlank
 import kotlin.String
 
 public data class SaveCategoryRequest(
+    @get:NotBlank
     public val title: String = ""
 )
